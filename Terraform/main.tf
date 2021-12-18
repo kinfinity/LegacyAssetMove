58d28282ebeaa010c1412646a0c344a5aeb14392
Egbewatt Kokou Mwangah Smith <ksupro1@gmail.com>
2021-12-18 15:18:35 +0100

    fix(s3-setup): fix bucketnames

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,14 +5,14 @@ provider "aws" {
 
 module "legacy-s3" {
   source = "./s3"
-  bucket_name = "legacy-s3"
+  bucket_name = "kokou-legacy-s3"
   project_name = "LegacyAssetMove"
   contact_email = "ksupro1@gmail.com"
 }
 
 module "production-s3" {
   source = "./s3"
-  bucket_name = "production-s3"
+  bucket_name = "kokou-production-s3"
   project_name = "LegacyAssetMove"
   contact_email = "ksupro1@gmail.com"
 
