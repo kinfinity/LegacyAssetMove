57f46328f7263308f3954d60c59357fe231a7414
Egbewatt Kokou Mwangah Smith <ksupro1@gmail.com>
2021-12-18 15:11:04 +0100

    fix(s3-setup): contact_email

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,12 +7,14 @@ module "legacy-s3" {
   source = "./s3"
   bucket_name = "legacy-s3"
   project_name = "LegacyAssetMove"
+  contact_email = "ksupro1@gmail.com"
 }
 
 module "production-s3" {
   source = "./s3"
   bucket_name = "production-s3"
   project_name = "LegacyAssetMove"
+  contact_email = "ksupro1@gmail.com"
 
 }
 
